742f529c029a1644e2c3d69e2a722d0f5e7e506c
Sergio Lucero <sergiolucero@gmail.com>
2023-06-28 10:46:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ from recorder import *
 
 st.set_page_config(layout="wide")
 st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
-st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhaAzr6jf5YvXano3_RQonb7on-4bJ6DaOtQFMa7Y&s')
-#st.write(f'(version {VERSION}). Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]')
+openai_logo()
+st.write(f'(version {VERSION}). Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]')
 audio = audiorecorder("Presione para grabar", "Grabando... presione para terminar")
 
 if len(audio) > 0:
