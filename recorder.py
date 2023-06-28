b023b4b1c350b8036779b24f910027ce66b8e1c7
Sergio Lucero <sergiolucero@gmail.com>
2023-06-28 14:23:48 -0700

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -1,11 +1,9 @@
-import boto3
-import openai
 import glob, os
-import tiktoken
 import time
 from datetime import datetime
 from pytz import timezone
 import streamlit as st
+from ai_lib import text_and_soap
 ###############################
 openai.api_key = st.secrets['OPEN_AI_KEY']
 AWS_KEY = st.secrets['AWS_KEY']
@@ -32,61 +30,6 @@ def chile_time():
     scl_time = datetime.now(scl).strftime('%Y-%m-%dT%H-%M-%S')
     return scl_time
 ###############################
-def tokens(text, completion_model):
-    encoding=tiktoken.encoding_for_model(completion_model)
-    return len(encoding.encode(text))
-
-def openai_transcribe(fn):
-    audio_file= open(fn, "rb")
-    try:
-        transcript = openai.Audio.transcribe(TRANSCRIPTION_MODEL, file=audio_file,
-            response_format="text",language="es")# text = transcript.to_dict()['text']
-    except Exception as e:
-        transcript = f'TRANSCRIPCIÓN FALLIDA: (FILE={fn}) \n ERROR={e}'
-    
-    return transcript
-    
-def soapit(text, completion_model = COMPLETION_MODEL):
-    
-    MAX_TOKENS = TOP_TOKENS-tokens(text, completion_model)
-    print('MAX_TOKENS:', MAX_TOKENS)
-    try:        # should use LangChain Prompts
-        response = openai.Completion.create(
-          model=completion_model,
-          prompt=f"resume este texto en formato médico SOAP:\n\n{text}",
-          temperature=1, max_tokens=MAX_TOKENS,
-          top_p=1.0,frequency_penalty=0.0,presence_penalty=0.0)
-        return response.to_dict()['choices'][0]['text']
-    except Exception as e:
-        transcript = f'RESUMEN SOAP FALLIDO: {e}'
-        return transcript
-        
-def summarize(text, completion_model = COMPLETION_MODEL):
-    MAX_TOKENS = TOP_TOKENS-tokens(text, completion_model)
-    try:
-        response = openai.Completion.create(
-            model=completion_model,
-            prompt=f"haz un punteo de este texto:\n\n{text}",
-            temperature=1,max_tokens=MAX_TOKENS,
-            top_p=1.0, frequency_penalty=0.0,presence_penalty=0.0
-            )
-    except Exception as e:
-        transcript = f'RESUMEN SOAP FALLIDO: {e}'
-
-    return response
-##################################################
-from langchain import OpenAI
-from langchain.docstore.document import Document
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains.summarize import load_summarize_chain
-
-def generate_response(txt):
-    llm = OpenAI(temperature=0, openai_api_key=openai.api_key)
-    text_splitter = CharacterTextSplitter()
-    texts = text_splitter.split_text(txt)
-    docs = [Document(page_content=t) for t in texts]
-    chain = load_summarize_chain(llm, chain_type='map_reduce')
-    return chain.run(docs)
 
 def s3_upload(files):
     s3 = boto3.client('s3', 
@@ -96,24 +39,6 @@ def s3_upload(files):
         filename = f'UPLOAD/{file}'
         s3.upload_fileobj(open(file,'rb'), FELIX_BUCKET, filename)
 
-def text_and_soap(fn):
-    t0=time.time()
-    text = openai_transcribe(fn)
-    t1=time.time()
-    print('TEXT:', text)
-    soap = soapit(text)
-    t2=time.time()
-    dts = [round(t1-t0,2), round(t2-t1,2)]
-
-    # uploading audio + 2 texts           (added 06-15)
-    txt_fn = fn.replace('.wav','.txt')
-    soap_fn = fn.replace('.wav','_soap.txt')
-
-    open(txt_fn, 'w').write(text)
-    open(soap_fn, 'w').write(soap)
-    s3_upload([fn, txt_fn, soap_fn])
-    
-    return text, soap, dts
     
 def output(audio_bytes):
     t0 = chile_time()
