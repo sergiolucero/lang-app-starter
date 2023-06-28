3d374d22c896acbf0b3ff9054b92b4a0f36e4bdd
Sergio Lucero <sergiolucero@gmail.com>
2023-06-28 09:36:10 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+from audiorcorder import audiorecorder
 
+st.set_page_config(layout="wide")
 st.title('🎈 CETRAM QuantMed LLM Doctor')
-
-st.write('Aquí vamos!')
+#st.write(f'(version {VERSION}). Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]')
+audio = audiorecorder("Presione para grabar", "Grabando... presione para terminar")
