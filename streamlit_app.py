ee5d1e59cd0bb8074dc864eb5c12ce2811cc854f
Sergio Lucero <sergiolucero@gmail.com>
2023-07-17 16:23:10 -0700

    removed diagnostico

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ if len(audio) > 0:
         with col1:
             st.header(f'AUDIO:')   # [dt={dts[0]} secs]
             st.write(text[:100]+'...')
-            diagnostico(text)
+            #diagnostico(text)
                     
         with col2:
             #st.write('add thumbs up/dn buttons to regenerate/accept!')
