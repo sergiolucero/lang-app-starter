5b3ee8cefc5e6492d846c9878af7d98a8f788da2
Sergio Lucero <sergiolucero@gmail.com>
2023-07-18 02:48:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from audiorecorder import audiorecorder
 from recorder import process
 from textlib import COMPLETION_MODEL, TRANSCRIPTION_MODEL, VERSION, API_KEY
-from linelib import fileread
+from linelib import fileread, linetab
 from datetime import datetime
 ##############################################
 st.set_page_config(layout="wide")
@@ -12,7 +12,7 @@ dropline = f'(version {VERSION}). Fecha={fecha}. Modelos: [complete={COMPLETION_
 st.write(dropline)
 ############################
 lines = fileread(fecha)
-tabs = {line.split(chr(10))[0]: tab(line) for line in lines}
+tabs = {line.split(chr(10))[0]: linetab(line) for line in lines}
 selected_tab = st.sidebar.selectbox("Paciente", list(tabs.keys()))
 tabs[selected_tab]()
 
