1f99eb5712de114437bfc96b7dfafa95dd2438e1
Sergio Lucero <sergiolucero@gmail.com>
2023-07-18 03:04:37 -0700

    Update recorder.py

--- recorder.py
+++ recorder.py
@@ -4,14 +4,14 @@ import streamlit as st
 from textlib import text_and_soap
 from timelib import chile_time
 ###############################
-def audiosave(fn, audio_bytes):
+def audiosave(fn, audio_bytes):    
     fw = open(fn, 'wb')
     fw.write(audio_bytes)
     fw.close()
 
-def process(audio_bytes):
+def process(audio_bytes, fecha, paciente):
     t0 = chile_time()
     fn = f'AUDIO/{t0}.wav'
     audiosave(fn, audio_bytes)
 
-    return text_and_soap(fn)
+    return text_and_soap(fn, fecha, paciente)
