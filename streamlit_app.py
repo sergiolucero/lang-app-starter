aad959f931b0f73353eaf0eb6817404ddf705bd1
Sergio Lucero <sergiolucero@gmail.com>
2023-07-18 02:15:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,45 +10,47 @@ st.set_page_config(layout="wide")
 st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
 #openai_logo()
 #st.write(f'API={API_KEY}')
+############################ tabbing 
+def tab(lines):   # should pass date+paciente
+    #html = open(filename).read()
+    slines = lines.split(chr(10))
+    head = slines[0]
+    body = slines[1:]
+    st.write(head)
+    st.write(body)
+    
+    audio = audiorecorder("Presione para grabar", "Grabando... presione para terminar")
+    
+    if len(audio) > 0:
+        st.audio(audio.tobytes())
+        with st.spinner('procesando...'):
+            text, soap, dts = process(audio.tobytes())
+            col1, col2 = st.columns(2)
+        
+            with col1:
+                st.header(f'TRANSCRIPCION AUDIO:')   # [dt={dts[0]} secs]
+                st.info(text)  # was write
+                #diagnostico(text)
+                        
+            with col2:
+                #st.write('add thumbs up/dn buttons to regenerate/accept!')
+                st.header(f'resumen SOAP:') # [dt={dts[1]} secs]
+                st.write(soap)
+                st.write('-'*80)
+                chunk_summary(text)
+
 # make this block a function call
 fecha = datetime.now().strftime('%Y-%m-%d')
 pfn = f'previo-{fecha}.txt'
 if os.path.exists(pfn):
     lines = open(pfn, encoding='utf-8').read()
     lines = lines.split('-'*80)
-    lines = [line.split(chr(10)) for line in lines]
+    #lines = [line.split(chr(10)) for line in lines]
     
 dropline = f'(version {VERSION}). Fecha={fecha}. Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
 st.write(dropline)
-############################ tabbing 
-def tab(ix, filename):
-    html = open(filename).read()
-    st.write(f"This is the content for Tab {ix+1}")
-    st.markdown(html)
-
 # Create a dictionary with tab names as keys and corresponding functions as values
-tabs = {f"Tab {ix+1}": tab(ix,filename) 
-        for ix,filename in enumerate(glob.glob('FICHAS/*18_07*'))}
-
-selected_tab = st.sidebar.selectbox("Select a tab", list(tabs.keys()))
+tabs = {line.split(chr(10))[0]: tab(line) for line in lines}
+selected_tab = st.sidebar.selectbox("Paciente", list(tabs.keys()))
 tabs[selected_tab]()
 
-audio = audiorecorder("Presione para grabar", "Grabando... presione para terminar")
-
-if len(audio) > 0:
-    st.audio(audio.tobytes())
-    with st.spinner('procesando...'):
-        text, soap, dts = process(audio.tobytes())
-        col1, col2 = st.columns(2)
-    
-        with col1:
-            st.header(f'TRANSCRIPCION AUDIO:')   # [dt={dts[0]} secs]
-            st.info(text)  # was write
-            #diagnostico(text)
-                    
-        with col2:
-            #st.write('add thumbs up/dn buttons to regenerate/accept!')
-            st.header(f'resumen SOAP:') # [dt={dts[1]} secs]
-            st.write(soap)
-            st.write('-'*80)
-            chunk_summary(text)
