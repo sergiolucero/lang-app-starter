a5d672d6002e8a055f1ad18ec9cb2a4a883a7167
Sergio Lucero <sergiolucero@gmail.com>
2023-07-18 02:57:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from audiorecorder import audiorecorder
-from recorder import process
 from textlib import COMPLETION_MODEL, TRANSCRIPTION_MODEL, VERSION, API_KEY
-from linelib import fileread, linetab
+from linelib import linetabs
 from datetime import datetime
 ##############################################
 st.set_page_config(layout="wide")
@@ -11,8 +9,7 @@ st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
 dropline = f'(version {VERSION}). Fecha={fecha}. Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
 st.write(dropline)
 ############################
-lines = fileread(fecha)
-tabs = {line.split(chr(10))[0]: linetab(line) for line in lines}
+tabs = linetabs(fecha)
 selected_tab = st.sidebar.selectbox("Paciente", list(tabs.keys()))
 tabs[selected_tab]()
 
