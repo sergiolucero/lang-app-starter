dd1ae772a36b40baba0d9c3e88a0e3741f730c23
Sergio Lucero <sergiolucero@gmail.com>
2023-08-23 08:43:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ from linelib import simple_recorder # was linetabs
 from datetime import datetime
 ##############################################
 st.set_page_config(layout="wide")
-#fecha = datetime.now().strftime('%Y-%m-%d')
-fecha = '2023-07-25'  # HARDCODED IN DAVIS!
+fecha = datetime.now().strftime('%Y-%m-%d')
+#fecha = '2023-07-25'  # HARDCODED IN DAVIS!
 
 st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
 dropline = f'(version {VERSION}). Fecha={fecha}. Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
