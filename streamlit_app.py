eaba543a895539a9e71e4b841ac979c2359a6762
Sergio Lucero <sergiolucero@gmail.com>
2023-08-23 08:47:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from textlib import COMPLETION_MODEL, TRANSCRIPTION_MODEL, VERSION
+from textlib import COMPLETION_MODEL, LANGUAGE, PROMPT, TRANSCRIPTION_MODEL, VERSION
 from linelib import simple_recorder # was linetabs
 from datetime import datetime
 ##############################################
@@ -8,7 +8,8 @@ fecha = datetime.now().strftime('%Y-%m-%d')
 #fecha = '2023-07-25'  # HARDCODED IN DAVIS!
 
 st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
-dropline = f'(version {VERSION}). Fecha={fecha}. Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
+dropline = f'(version {VERSION}). Fecha={fecha}. \n Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
+dropline += f'Language: {LANGUAGE}. Prompt: {PROMPT}'
 st.write(dropline)
 ############################
 # FUTURE: https://blog.streamlit.io/how-to-build-the-streamlit-webrtc-component/   
