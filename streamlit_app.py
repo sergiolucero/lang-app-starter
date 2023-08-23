a42fc03b6f56ca56989ffa00e3f47e527c26e02a
Sergio Lucero <sergiolucero@gmail.com>
2023-08-23 09:11:20 -0400

    created REVIEW tab

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,14 @@ from datetime import datetime
 st.set_page_config(layout="wide")
 fecha = datetime.now().strftime('%Y-%m-%d')
 #fecha = '2023-07-25'  # HARDCODED IN DAVIS!
-
-st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
-dropline = f'(version {VERSION}). Fecha={fecha}. \n Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
-dropline += f'Language: {LANGUAGE}. Prompt: {PROMPT}'
-st.write(dropline)
-############################
-# FUTURE: https://blog.streamlit.io/how-to-build-the-streamlit-webrtc-component/   
-simple_recorder()
+tab1, tab2 = st.tabs(['Grabación','Revisión'])
+with tab1:
+    st.title('👨‍⚕️CETRAM QuantMed LLM Doctor🤖')
+    dropline = f'(version {VERSION}). Fecha={fecha}. \n Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
+    dropline += f'Language: {LANGUAGE}. Prompt: {PROMPT}'
+    st.write(dropline)
+    ############################
+    # FUTURE: https://blog.streamlit.io/how-to-build-the-streamlit-webrtc-component/   
+    simple_recorder()
+with tab2:
+    st.write('FUTURE: list contents of cetram-felix/AUDIO')
