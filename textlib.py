fcad16ecc23368a2753006209c43c217db04d0c2
Sergio Lucero <sergiolucero@gmail.com>
2023-08-29 19:50:21 -0400

    Update textlib.py

--- textlib.py
+++ textlib.py
@@ -4,6 +4,7 @@ import tiktoken
 import time
 import json
 import streamlit as st
+from datetime import datetime
 
 from awslib import s3_upload
 
@@ -28,9 +29,11 @@ os.environ['OPENAI_API_KEY'] = st.secrets['OPEN_AI_KEY']
 openai.api_key = os.environ['OPENAI_API_KEY']
 API_KEY = openai.api_key
 ##############################################
+fecha = datetime.now().strftime('%Y-%m-%d')
+
 droplines = [f'(version {VERSION}). Fecha={fecha}. Modelos: [complete={COMPLETION_MODEL}, transcribe={TRANSCRIPTION_MODEL}]'
             f'Language: {LANGUAGE}. Prompt: {PROMPT}']
-    
+##############################################    
 def tokens(text, completion_model):
     encoding = tiktoken.encoding_for_model(completion_model)
     return len(encoding.encode(text))
